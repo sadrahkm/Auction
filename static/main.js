d11059f00036107186f849872a8a186701bbf55a
sabahonarmand <sabahonarmand79@gmail.com>
2021-12-15 00:35:53 +0330

    add some changes to callNewProduct

--- static/main.js
+++ static/main.js
@@ -1,17 +1,29 @@
 /* eslint-disable no-undef */
 $(document).ready(function () {
   const socket = io.connect("http://localhost:3000/");
+
   $("#login-Btn").click((event) => {
     event.preventDefault();
     let username = $("#username").val();
     let password = $("#password").val();
     if (username === "" | password === "") {
-      alert("Oops! Please, enter username and password⚠")
+      alert("Oops! Please, enter username and password⚠️")
     }
     else {
       socket.emit("login", { name: username, password: password })
     }
   });
+
+  $("submit_price_id").click((event) => {
+    event.preventDefault();
+    let proposedPrice = $("price_value_id").val();
+    if (proposedPrice === "") {
+      alert("Oops! Please, enter your proposed price⚠️");
+    }
+    else {
+      socket.emit("getNewPrice", { price: proposedPrice });
+    }
+  })
 
   // ----------------------------------------------
 
@@ -29,5 +41,7 @@ $(document).ready(function () {
     $("#product_name").text(data.name);
     $("#base_price").text(data.price);
     $("#description").text(data.description);
+    $("#start_auction_id").html("مزایده آغاز شد");
+    $("#start_auction_id").css("background-color", "#32CD32");
   });
 });
